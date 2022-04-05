9070eb00ba3832a711a1909cda188ed08914cc16
Wiston_g <gonzalesjair54@gmail.com>
2022-04-05 17:41:26 -0500

    crud estableciendo conexion a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginController } from './login/login.controller';
@@ -7,8 +8,19 @@ import { RegisterService } from './register/register.service';
 import { RegisterModule } from './register/register.module';
 
 @Module({
-  imports: [RegisterModule],
-  controllers: [AppController, LoginController, RegisterController],
-  providers: [AppService, RegisterService],
+  imports: [RegisterModule, 
+    TypeOrmModule.forRoot({
+      "type": "mysql",
+      "host": "localhost",
+      "port": 3306,
+      "username": "root",
+      "password": "",
+      "database": "test",
+      "entities": ["dist/**/*.entity{.ts,.js}"],
+      "synchronize": true
+    })
+    ],
+      controllers: [AppController, LoginController, RegisterController],
+      providers: [AppService, RegisterService],
 })
 export class AppModule {}
